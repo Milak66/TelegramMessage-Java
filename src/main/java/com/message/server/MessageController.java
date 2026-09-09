33e3c8e917bb43f928a77c46f2316b3b20ae03b1
Aleksey <134363714+Milak66@users.noreply.github.com>
2026-09-09 19:21:55 +0200

    test1

--- src/main/java/com/message/server/MessageController.java
+++ src/main/java/com/message/server/MessageController.java
@@ -1,6 +1,7 @@
 package com.message.server;
 
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
