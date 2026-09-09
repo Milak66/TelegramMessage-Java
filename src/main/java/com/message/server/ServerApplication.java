789b99315b71162ac695d2453950e4f1cd91c05f
Aleksey <134363714+Milak66@users.noreply.github.com>
2026-09-09 18:24:21 +0200

    Telegram message 1.1.3

--- src/main/java/com/message/server/ServerApplication.java
+++ src/main/java/com/message/server/ServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServerApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
